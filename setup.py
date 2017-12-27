d5101f201b8a7f809e0ca792278adfd581612ad9
Nick Barber <nickwbarber@gmail.com>
2017-12-27 15:36:00 -0600

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='hiltnlp',
+    version='0.1',
+    description='NLP tools for use with HiLT-style GATE annotation documents',
+    url='http://github.com/nickwbarber/hiltnlp',
+    author='Nick Barber',
+    author_email='nickwbarber@gmail.com',
+    license='MIT',
+    packages=['hiltnlp'],
+    install_requires=[
+        'gatenlp',
+    ],
+    zip_safe=False
+)
